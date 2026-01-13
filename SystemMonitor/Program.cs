4b5fcf1a28fd0647767cb855a45d44ffde5f4040
DigiLeaf <leiforth01@hotmail.com>
2026-01-13 17:18:56 -0600

    Add project files.

---
+++ SystemMonitor/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Identity.Client.Utils;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! This is the beginning of my System Resource Monitor");
+    }
+}
