81a13543871aaa663c7231bde358f88aaeb417e6
DigiLeaf <leiforth01@hotmail.com>
2026-01-14 19:10:01 -0600

    Add Basic resource reading loop

--- SystemMonitor/Program.cs
+++ SystemMonitor/Program.cs
@@ -1,11 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Identity.Client.Utils;
+﻿using Microsoft.Identity.Client.Utils;
+using System.Diagnostics;
+using System.IO;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! This is the beginning of my System Resource Monitor");
+        Console.WriteLine("Hello, World! This is the beginning of my System Resource Monitor"); 
+        
+        //Counters
+                                                    //Category Name     Counter Name       Instance Name
+        PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        //initializing cpu performance counter.
+        cpuCounter.NextValue();
+        Thread.Sleep(500);
+
+                                                        //Category Name    Counter Name 
+        PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available Mbytes");
+
+        PerformanceCounter diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+
+            //Get data of a known drive drive available
+        DriveInfo mainDrive = new DriveInfo("C");
+
+
+        long totalDriveSpace;
+        long freeDriveSpace;
+
+        while (true) {
+            //Drive Data
+             totalDriveSpace = mainDrive.TotalSize;
+             freeDriveSpace = mainDrive.AvailableFreeSpace;
+            
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine($"CPU Usage:    {cpuCounter.NextValue():0.##} %");
+            Console.WriteLine($"Availabe Ram: {ramCounter.NextValue():0.##} MB");
+            Console.WriteLine($"Disk Usage:   {diskCounter.NextValue():0.##} %");
+            Console.WriteLine($"Total Disk:   {totalDriveSpace / 1024 / 1024 / 1024} GB");
+            Console.WriteLine($"Free Disk:    {freeDriveSpace / 1024 / 1024 / 1024} GB");
+
+            //Pause between system readings
+            Thread.Sleep(2000);
+
+        }
     }
 }
