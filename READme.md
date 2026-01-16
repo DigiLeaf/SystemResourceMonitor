e4baa13155bf610c2243beb982a308a2836947b7
DigiLeaf <leiforth01@hotmail.com>
2026-01-16 15:51:13 -0600

    Add Readme

---
+++ READme.md
@@ -0,0 +1,30 @@
+# System Resource Monitor
+System Resource Monitor is a lightweight C#/.NET console application that periodically monitors local system resources, including CPU, memory, and disk usage.
+Collected metrics are stored in an Azure SQL Database to enable historical analysis, reporting, and alerting.
+
+## Features (Planned)
+- Monitors local system resources:
+	- CPU usage
+	- Memory (RAM) usage
+	- Disk Usage
+- Periodic data collection at configurable intervals
+- Persistent storage using Azure SQL Database
+- Historical data querying and basic reporting
+- Threshold-based alerts for abnormal resource usage
+- Export monitoring data to Excel files
+
+## Tech Stack
+- **Lanuage**
+	- C#
+
+- **Runtime**
+  - .NET (Console Application)
+
+- **Database**
+  - Azure SQL Database
+
+- **Data Access** 
+  - Entity Framework Core
+    
+- **Version Control**
+  - Git & Github
--- SystemMonitor/Program.cs
+++ SystemMonitor/Program.cs
@@ -7,43 +7,48 @@ class Program
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! This is the beginning of my System Resource Monitor"); 
-        
-        //Counters
-                                                    //Category Name     Counter Name       Instance Name
-        PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-        //initializing cpu performance counter.
-        cpuCounter.NextValue();
-        Thread.Sleep(500);
+        Console.WriteLine("Hello, World! This is the beginning of my System Resource Monitor");
+       //Counter Assignment
+                                                            //Category Name     Counter Name       Instance Name
+       PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+       //initializing cpu performance counter.
+       cpuCounter.NextValue();
+       Thread.Sleep(500);
 
-                                                        //Category Name    Counter Name 
+                                                            //Category Name    Counter Name 
         PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available Mbytes");
 
         PerformanceCounter diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
 
-            //Get data of a known drive drive available
+        //Get data of a known drive drive available
         DriveInfo mainDrive = new DriveInfo("C");
-
 
         long totalDriveSpace;
         long freeDriveSpace;
+        while (true)
+        {
+             try
+             {
+                 //Drive Data
+                 totalDriveSpace = mainDrive.TotalSize;
+                 freeDriveSpace = mainDrive.AvailableFreeSpace;
 
-        while (true) {
-            //Drive Data
-             totalDriveSpace = mainDrive.TotalSize;
-             freeDriveSpace = mainDrive.AvailableFreeSpace;
-            
 
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine($"CPU Usage:    {cpuCounter.NextValue():0.##} %");
-            Console.WriteLine($"Availabe Ram: {ramCounter.NextValue():0.##} MB");
-            Console.WriteLine($"Disk Usage:   {diskCounter.NextValue():0.##} %");
-            Console.WriteLine($"Total Disk:   {totalDriveSpace / 1024 / 1024 / 1024} GB");
-            Console.WriteLine($"Free Disk:    {freeDriveSpace / 1024 / 1024 / 1024} GB");
+                 Console.WriteLine("------------------------------------");
+                 Console.WriteLine($"CPU Usage:    {cpuCounter.NextValue():0.##} %");
+                 Console.WriteLine($"Availabe Ram: {ramCounter.NextValue():0.##} MB");
+                 Console.WriteLine($"Disk Usage:   {diskCounter.NextValue():0.##} %");
+                 Console.WriteLine($"Total Disk:   {totalDriveSpace / 1024 / 1024 / 1024} GB");
+                 Console.WriteLine($"Free Disk:    {freeDriveSpace / 1024 / 1024 / 1024} GB");
+             }
+             catch (Exception ex){
+                 Console.Error.WriteLine($"An error has occured: {ex.Message}");
+             }
+                //Pause between system readings
+                Thread.Sleep(2000);
 
-            //Pause between system readings
-            Thread.Sleep(2000);
+         }
+        
 
-        }
     }
 }
